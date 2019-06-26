3be037373bcf9c62f247ddbe873282c73d29f722
Hartwell Fong <47191469+Physicslibrary@users.noreply.github.com>
2019-06-25 23:08:13 -0400

    Update learn.swift

--- learn.swift
+++ learn.swift
@@ -1,14 +1,14 @@
 /*
  
- ARKit-Stereoscope
+ARKit-Stereoscope
  
- https://physicslibrary.github.io/ARKit-Stereoscope/
+https://physicslibrary.github.io/ARKit-Stereoscope/
  
- MIT License
+MIT License
  
- Copyright (c) 2019 Hartwell Fong
+Copyright (c) 2019 Hartwell Fong
  
- Permission is hereby granted, free of charge, to any person
+Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation
 files (the "Software"), to deal in the Software without restriction,
 including without limitation the rights to use, copy, modify, merge,
@@ -16,53 +16,55 @@ publish, distribute, sublicense, and/or sell copies of the Software,
 and to permit persons to whom the Software is furnished to do so, subject
 to the following conditions:
  
- The above copyright notice and this permission notice shall be included
+The above copyright notice and this permission notice shall be included
 in all copies or substantial portions of the Software.
  
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
 OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  
- Updated Mar 8, 2019.
- This playground uses ARKit and SceneKit to set up scene, read a 3D file, read 2 images,
+Updated Mar 8, 2019.
+
+This playground uses ARKit and SceneKit to set up scene, read a 3D file, read 2 images,
 and attact a virtual camera for lefteye to ARKit iPad camera righteye to make a stereoscopic image.
- Hardware:
+
+Hardware:
  
- Apple 9.7" iPad (tested on 2018 model, should work on 2017 9.7" iPad, A9 CPU or higher for ARKit)
+Apple 9.7" iPad (tested on 2018 model, should work on 2017 9.7" iPad, A9 CPU or higher for ARKit)
  
- The OWL Stereoscopic Viewer from The London Stereoscopic Company Ltd
- https://www.londonstereo.com
+The OWL Stereoscopic Viewer from The London Stereoscopic Company Ltd
+https://www.londonstereo.com
  
- Software:
+Software:
  
- Apple Swift Playgrounds (3.0) from iOS App Store
+Apple Swift Playgrounds (3.0) from iOS App Store
  
- Tips:
+Tips:
  
- If frame rate <60Hz, press iPad HOME button, return to Swift Playgrounds
+If frame rate <60Hz, press iPad HOME button, return to Swift Playgrounds
  
- Experiment with smaller size OBJ files
- 15MB model.obj was generated from resolution=200 (NASA Moon Trek)
+Experiment with smaller size OBJ files
+15MB model.obj was generated from resolution=200 (NASA Moon Trek)
  
- The codes doesn't look for a flat plane to put virtual objects on, instead the initial position
+The codes doesn't look for a flat plane to put virtual objects on, instead the initial position
 of the iPad is the world origin when "Run My Code".
 
- Virtual objects are positioned and oriented to this world origin (with righteye.debugOptions on,
+Virtual objects are positioned and oriented to this world origin (with righteye.debugOptions on,
 the world origin is an XYZ or RGB axis).
 
 Credits
  
- TychoSkymap.t4_04096x02048.jpg
- NASA/Goddard Space Flight Center Scientific Visualization Studio
- https://svs.gsfc.nasa.gov/3442
+TychoSkymap.t4_04096x02048.jpg
+NASA/Goddard Space Flight Center Scientific Visualization Studio
+https://svs.gsfc.nasa.gov/3442
 
- model.obj, terrain.mtl, and texture.png
- https://trek.nasa.gov/moon
+model.obj, terrain.mtl, and texture.png
+https://trek.nasa.gov/moon
  
- */
+*/
 
 import ARKit
 import PlaygroundSupport
